c733295eba8b03f8abe69c03a240476efbbd99bb
Douglas Edric Stanley <douglas@abstractmachine.net>
2016-02-15 00:16:55 +0100

    Remove Fungus files not essential to project

--- Assets/Tests/UI/TestFlags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TestFlags : MonoBehaviour 
-{
-	public bool messageReceived { get; set; }
-
-	public bool writeCommandsFinished { get; set; }
-}
